39058ea3daa2595ca75428fc2d3581ddb209327e
Julian <concordofrancors@protonmail.ch>
2023-12-19 17:04:50 +1100

    Added all words for Lesson one

--- scripts/csv-to-keyed-json.py
+++ scripts/csv-to-keyed-json.py
@@ -4,6 +4,7 @@ import csv
 data = pd.read_csv('../words.csv')
 df = pd.DataFrame(data)
 df = df.sort_values(by=["name"], ascending=True)
+df.to_csv('../words.csv', index=False)
 
 json_file = df.set_index("name").to_json(orient='index',indent=4)
 print(json_file)
