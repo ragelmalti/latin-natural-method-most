deeeed5594f934f10ebeb310c3449c47a5227a75
Julian <concordofrancors@protonmail.ch>
2023-12-19 17:34:27 +1100

    Fixed sequential update issue

--- latin.js
+++ latin.js
@@ -2,9 +2,12 @@ function selectWord(e) {
     // Untoggles words that have already been highlighted
     // Solution taken from: https://stackoverflow.com/a/22270709
     existsHighlighted = document.getElementsByClassName('highlighted');
+    defParagraph = document.getElementById('definition').getElementsByTagName('p')[0];
+    /* Redundant variables
     wordSpan = document.getElementById('defined_word');
     defSpan = document.getElementById('def');
     grammarSpan = document.getElementById('grammar');
+    */
     //console.log(definitionDiv);
     if (e.target.classList.contains('word')) {
         [].forEach.call(existsHighlighted, function(el) {
@@ -14,7 +17,6 @@ function selectWord(e) {
         // Figure out how to remove forbidden chars:
         // ' , . : ?
         let selectedWord = e.target.innerHTML;
-        wordSpan.textContent = selectedWord + ":";
         
         // Select definition from json file
         fetch('../words.json')
@@ -24,21 +26,25 @@ function selectWord(e) {
         .then(function(json) {
             // Solution taken from: https://stackoverflow.com/a/1946247
             try {
+                defParagraph.textContent = "";
                 let definition = json[selectedWord.toLowerCase()].definition;
-                defSpan.textContent = definition;
+                let grammar = json[selectedWord.toLowerCase()].grammar;
+                let wordSpan = document.createElement("span");
+                wordSpan.classList.add("defined_word");
+                let defSpan = document.createElement("span");
+                defSpan.classList.add("def");
+                wordSpan.innerText = selectedWord + ": ";
+                defSpan.innerText = definition;
+                defParagraph.appendChild(wordSpan);
+                defParagraph.appendChild(defSpan);
+                
+                // ADD GRAMMAR AT A LATER TIME
+                //let grammarSpan = document.createElement("span");
+
             }
             catch {
                 console.log("Error, undefined word!");
                 defSpan.textContent = "undefined"
-            }
-
-            try {
-                let grammar = json[selectedWord.toLowerCase()].grammar;
-                grammarSpan.textContent = grammar;
-            }
-            catch {
-                grammarSpan.textContent = "";
-                console.log("Error, undefined grammar!");
             }
         })
     }
@@ -60,3 +66,12 @@ function addSpanTagsToLatinWords() {
 }
 
 addSpanTagsToLatinWords();
+/*
+let defParagraph = document.getElementById('definition').getElementsByTagName('p')[0];
+let word = document.createElement("span");
+word.classList.add("defined_word");
+word.innerText = "Woah, hey guys, welcome to EB Games!"
+defParagraph.textContent = "";
+defParagraph.appendChild(word);
+console.log(defParagraph);
+*/
